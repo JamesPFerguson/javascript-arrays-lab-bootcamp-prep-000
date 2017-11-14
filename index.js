e5db729ff2cace673a641e18f1ae8d9585bc1ec4
James Ferguson <ncjamesferguson@gmail.com>
2017-11-14 15:32:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ const app = "I don't do much.";
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
-  
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+function appendKitten(name) {
+  var freshkit = [kittens, ...name]
 }
